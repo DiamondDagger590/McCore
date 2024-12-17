3c2c0e6abf0f8be367bee39c9190836ed2db00bb
DiamondDagger590 <diamonddagger590@gmail.com>
2024-12-16 19:14:45 -0500

    Update to 1.21.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     val intellijAnnotationVersion = "12.0"
     compileOnlyApi("com.intellij:annotations:$intellijAnnotationVersion")
 
-    val paperVersion = "1.21-R0.1-SNAPSHOT"
+    val paperVersion = "1.21.4-R0.1-SNAPSHOT"
     compileOnlyApi("io.papermc.paper:paper-api:$paperVersion")
 
     val cloudMinecraftVersion = "2.0.0-beta.9"
