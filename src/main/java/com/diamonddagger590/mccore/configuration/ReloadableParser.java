05f5f8102c06c7c31494e9b9363f618f7e79268f
DiamondDagger590 <diamonddagger590@gmail.com>
2025-02-24 17:45:28 -0500

    Add reloadable parser

---
+++ src/main/java/com/diamonddagger590/mccore/configuration/ReloadableParser.java
@@ -0,0 +1,17 @@
+package com.diamonddagger590.mccore.configuration;
+
+import com.diamonddagger590.mccore.parser.Parser;
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.route.Route;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A type of {@link ReloadableContent} that contains a {@link Parser}
+ * to be used for equations.
+ */
+public class ReloadableParser extends ReloadableContent<Parser> {
+
+    public ReloadableParser(@NotNull YamlDocument yamlDocument, @NotNull Route route) {
+        super(yamlDocument, route, (yamlDocument1, route1) -> new Parser(yamlDocument1.getString(route1)));
+    }
+}
