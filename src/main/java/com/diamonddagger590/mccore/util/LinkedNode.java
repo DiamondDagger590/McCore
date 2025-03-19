ae52869852a3a0f006cab44ef6c327df076cafa9
DiamondDagger590 <diamonddagger590@gmail.com>
2025-03-18 20:19:58 -0400

    Minor framework updates

--- src/main/java/com/diamonddagger590/mccore/util/LinkedNode.java
+++ src/main/java/com/diamonddagger590/mccore/util/LinkedNode.java
@@ -2,6 +2,11 @@ package com.diamonddagger590.mccore.util;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A linked node allows for linking data together in a sequential manner.
+ *
+ * @param <E> The data type stored inside this node.
+ */
 public class LinkedNode<E> {
 
     private final E nodeValue;
@@ -16,15 +21,40 @@ public class LinkedNode<E> {
         this.nextNode = nextNode;
     }
 
+    /**
+     * Gets the value stored in this node.
+     *
+     * @return The value stored in this node.
+     */
     @NotNull
     public E getNodeValue() {
         return nodeValue;
     }
 
+    /**
+     * Sets the node that is the next in the linked chain.
+     *
+     * @param nextNode The node that is next in the linked chain.
+     */
     public void setNext(@NotNull LinkedNode<E> nextNode) {
         this.nextNode = nextNode;
     }
 
+    /**
+     * Checks to see if there is a next node in the linked chain.
+     *
+     * @return {@code true} if there is a next node in the linked chain.
+     */
+    public boolean hasNext() {
+        return nextNode != null;
+    }
+
+    /**
+     * Gets the next node in the linked chain.
+     *
+     * @return The next node in the linked chain.
+     * @throws NullPointerException if there is not a next node in the linked chain.
+     */
     @NotNull
     public LinkedNode<E> getNextNode() {
         if (nextNode == null) {
