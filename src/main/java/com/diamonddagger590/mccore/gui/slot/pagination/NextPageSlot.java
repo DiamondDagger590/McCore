fdd29ce8b9a3efea5573571e6397f9fa92b74eb7
DiamondDagger590 <diamonddagger590@gmail.com>
2025-07-29 19:39:12 -0400

    Changes to slots to make them more extensible

--- src/main/java/com/diamonddagger590/mccore/gui/slot/pagination/NextPageSlot.java
+++ src/main/java/com/diamonddagger590/mccore/gui/slot/pagination/NextPageSlot.java
@@ -15,7 +15,7 @@ import java.util.Set;
  * This is a slot to be used in {@link PaginatedGui}s to allow for going
  * to the next page of the gui.
  */
-public abstract class NextPageSlot<P extends CorePlayer> extends Slot<P> {
+public abstract class NextPageSlot<P extends CorePlayer> implements Slot<P> {
 
     private final Set<Class<?>> VALID_GUIS = Set.of(PaginatedGui.class);
 
